aac99af69622772c4340d29efb5083aeb1e98e78
Saurabh Yadav <sy24abe@herts.ac.uk>
2025-12-07 15:50:29 +0000

    T.Test & Visualisation

--- Script.R
+++ Script.R
@@ -11,7 +11,9 @@ print(colnames(cr))
 # To open the imported dataset and display it in a new tab.
 View(cr)
 
-# Loading libraries to change format from wide to long
+# Install & Loading libraries to change format from wide to long
+install.packages("tidyr")
+install.packages("dplyr")
 library(tidyr)
 library(dplyr)
 
@@ -61,3 +63,65 @@ print (cr_long$Region_in_Jamaica[64])
 print (cr_long$Region_in_Jamaica[1])
 print (cr_long$City[64])
 print (cr_long$City[1])
+
+
+# Taking only North and South regions
+cr_test <- cr_long %>%
+  filter(Region %in% c("North", "South")) %>%
+  mutate(
+    Region = factor(Region, levels = c("South", "North"))
+  )
+
+# Number of observations per region
+table(cr_test$Region)
+
+# Mean crashes by region
+aggregate(Crashes ~ Region, data = cr_test, mean, na.rm = TRUE)
+
+# Independent t-test
+t.test(Crashes ~ Region, data = cr_test)
+
+# Wilcoxon test (non-normal data)
+wilcox.test(Crashes ~ Region, data = cr_test)
+
+# File Name for Exporting Bar Plot
+png("Visualisation.png")
+
+# Total crashes by region
+region_sum <- aggregate(Crashes ~ Region, data = cr_long, sum, na.rm = TRUE)
+
+# Create colors equal to number of regions
+cols <- rainbow(nrow(region_sum))
+
+# Create bar plot with y-axis
+bp <- barplot(
+  region_sum$Crashes,
+  names.arg = rep("", nrow(region_sum)),
+  col = rainbow(nrow(region_sum)),
+  main = "Distribution of Crashes by Region",
+  xlab = "Region",
+  ylab = "Number of Crashes",
+  ylim = c(0, max(region_sum$Crashes) * 1.15)
+)
+
+# Add values on bars
+text(
+  x = bp,
+  y = region_sum$Crashes,
+  labels = format(region_sum$Crashes, big.mark = ","),
+  pos = 3,
+  cex = 0.8
+)
+
+# Legend
+legend(
+  "topright",
+  legend = region_sum$Region,
+  fill = rainbow(nrow(region_sum)),
+  bty = "o",
+  bg = "white",
+  box.col = "black"
+)
+
+# this closes the file after writing
+dev.off()
